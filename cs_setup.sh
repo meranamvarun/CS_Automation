61ea4f479b2e4b2e987d6511b3b79ac0e62ca99d
Varun M S <varun.cse.nitap@gmail.com>
2017-11-09 02:45:57 +0530

    Update cs_setup.sh

--- cs_setup.sh
+++ cs_setup.sh
@@ -4,12 +4,10 @@ sudo apt-get update -y && sudo apt-get upgrade -y && sudo apt-get install lib32g
 password="steam"
 pass=$(perl -e 'print crypt($ARGV[0], "password")' $password)
 sudo useradd -m -p $pass steam
-sudo su steam
-cd /home/steam
-mkdir SteamCMD
-chown steam:steam SteamCMD
-cd steamCMD && wget http://media.steampowered.com/installer/steamcmd_linux.tar.gz
-tar -xvzf steamcmd_linux.tar.gz
-chown steam:steam steamcmd_linux
-rm steamcmd_linux.tar.gz
+sudo mkdir -p /home/steam/SteamCMD
+sudo wget -O /home/steam/SteamCMD  http://media.steampowered.com/installer/steamcmd_linux.tar.gz
+sudo tar -xvzf /home/steam/SteamCMD/steamcmd_linux.tar.gz -C /home/steam/SteamCMD
+sudo chown -R steam:steam /home/steam/SteamCMD
+sudo rm /home/steam/SteamCMD/steamcmd_linux.tar.gz
+sudo su - steam
 
